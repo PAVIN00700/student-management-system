2373123dafcbf16c111595def2c16796dbcd124e
pavin <pavin.palanisaamy@gmail.com>
2026-04-21 19:52:59 +0530

    Update API URL to production backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://outstanding-learning-production-6982.up.railway.app/api",
 });
 
 // 🔥 Attach token to every request
